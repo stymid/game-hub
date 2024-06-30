f02ffa4e0257dc5b00c3e9f1ac6b7127af773071
MAHDI SABAHI <mrmahdisabahi@gmail.com>
2024-06-30 17:32:05 +0330

    Built an responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`, //1024px
+        }}
+      >
+        <GridItem area={"nav"} bg={"coral"}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+          {/* only show on lg devices and above */}
+          <GridItem area={"aside"} bg={"gold"}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area={"main"} bg={"dodgerblue"}>
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;

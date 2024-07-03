fb7451ade2167d0ef1a43679c69c85ca012ced90
MAHDI SABAHI <mrmahdisabahi@gmail.com>
2024-07-03 23:27:30 +0330

    Show loading skeleton

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,16 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <>
+      <Card width="300px" borderRadius={10} overflow={"hidden"}>
+        <Skeleton height="200px"></Skeleton>
+        <CardBody>
+          <SkeletonText />
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default GameCardSkeleton;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,24 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGmes";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const Skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
+      {isLoading && (
+        <SimpleGrid
+          columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
+          padding={10}
+          spacing={10}
+        >
+          {Skeletons.map((skeleton) => (
+            <GameCardSkeleton key={skeleton} />
+          ))}
+        </SimpleGrid>
+      )}
       {error && <Text> {error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}

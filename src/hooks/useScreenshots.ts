e11d726d9422361f396026fb10e317177aa14bb7
Mahdi Sabahi <mrmahdisabahi@gmail.com>
2024-08-13 02:04:32 +0330

    Fixing error at build time

--- src/hooks/useScreenshots.ts
+++ src/hooks/useScreenshots.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
-import Screenshot from "../entities/ScreenShot";
+import Screenshot from "../entities/Screenshot";
 
 const useScreenshots = (gameId: number) => {
   const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
